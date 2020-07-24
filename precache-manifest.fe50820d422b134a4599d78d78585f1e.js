c905a49ccca5db0e332599fdbd0594cb37441b2f
Jesse Omoefe <jesseefe@gmail.com>
2020-07-24 17:01:14 +0100

    Updates

---
+++ precache-manifest.fe50820d422b134a4599d78d78585f1e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77566875f52e9a32f3deb6d7c2c1e4eb",
+    "url": "/Gotanyalo-React/index.html"
+  },
+  {
+    "revision": "8cf96a50917e5c577be1",
+    "url": "/Gotanyalo-React/static/css/main.cf3e3228.chunk.css"
+  },
+  {
+    "revision": "f05ed699a5132db13cb1",
+    "url": "/Gotanyalo-React/static/js/2.6fed668e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Gotanyalo-React/static/js/2.6fed668e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf96a50917e5c577be1",
+    "url": "/Gotanyalo-React/static/js/main.4e4dfbfb.chunk.js"
+  },
+  {
+    "revision": "df56e945c3ab9a4bbb6a",
+    "url": "/Gotanyalo-React/static/js/runtime-main.fe6451a1.js"
+  },
+  {
+    "revision": "0f7eec04d00cbd9ec784007500b8c282",
+    "url": "/Gotanyalo-React/static/media/cactus.0f7eec04.jpg"
+  },
+  {
+    "revision": "b2a13502bad8c6dd45876b9a91d8cf84",
+    "url": "/Gotanyalo-React/static/media/house.b2a13502.jpg"
+  },
+  {
+    "revision": "3fc26379a17e464e267e6cd5b34bbeba",
+    "url": "/Gotanyalo-React/static/media/onion.3fc26379.jpg"
+  },
+  {
+    "revision": "ddda4d44a1962825250dc11520b558a6",
+    "url": "/Gotanyalo-React/static/media/wrench.ddda4d44.jpg"
+  }
+]);
